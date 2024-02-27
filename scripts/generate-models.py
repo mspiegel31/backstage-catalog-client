084ef67c88cc3c0025eab10896b6ac552c3980c2
Mike Spiegel <mspiegel31@gmail.com>
2024-02-27 12:50:46 -0500

    stubbing model fetch code

--- scripts/generate-models.py
+++ scripts/generate-models.py
@@ -4,15 +4,29 @@ from urllib.parse import urljoin
 import httpx
 
 api_request_path = "/repos/backstage/backstage/contents/"
-schema_path = "packages/catalog-model/src/schema"
+schema_path = "packages/catalog-model/src"
+
 
 schema_request = urljoin(api_request_path, schema_path)
 
 
+async def get_schema_tree(client: httpx.AsyncClient, schema_request: str):
+    resp = await client.get(schema_request)
+    schemas = list(filter(lambda x: x["name"] == "schema", resp.json())).pop()
+    tree_request = httpx.URL(schemas["git_url"]).copy_add_param("recursive", "true")
+    tree_resp = await client.get(tree_request)
+    return tree_resp.json()
+
+
 async def main():
     async with httpx.AsyncClient(base_url="https://api.github.com/") as client:
-        resp = await client.get(schema_request)
-        print(resp.json())
+        client.headers.update(
+            {
+                "Accept": "application/vnd.github.raw+json",
+                "User-Agent": "backstage_catalog_client",
+            }
+        )
+        tree = await get_schema_tree(client, schema_request)
 
 
 if __name__ == "__main__":
