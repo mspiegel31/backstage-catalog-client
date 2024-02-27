4ca41b1d8c31e5495f7a09e78444c6eb29aa57fa
Mike Spiegel <mspiegel31@gmail.com>
2024-02-27 13:26:30 -0500

    more stubbing

--- scripts/generate-models.py
+++ scripts/generate-models.py
@@ -1,7 +1,11 @@
 import asyncio
+import base64
+from pathlib import Path
 from urllib.parse import urljoin
 
 import httpx
+
+ROOT = Path(__file__).parent.parent
 
 api_request_path = "/repos/backstage/backstage/contents/"
 schema_path = "packages/catalog-model/src"
@@ -18,6 +22,30 @@ async def get_schema_tree(client: httpx.AsyncClient, schema_request: str):
     return tree_resp.json()
 
 
+async def write_schemas(client: httpx.AsyncClient, tree_resp: list[dict]):
+    base_path = ROOT / Path("schemas")
+    if not base_path.exists():
+        base_path.mkdir()
+
+    for leaf in tree_resp["tree"]:
+        if leaf["type"] == "tree":
+            file_path = base_path / Path(leaf["path"])
+            if not file_path.exists():
+                file_path.mkdir()
+        if leaf["type"] == "blob":
+            file_path = base_path / Path(leaf["path"])
+            client.headers.update(
+                {
+                    "Accept": "application/vnd.github.object+json",
+                    "User-Agent": "backstage_catalog_client",
+                }
+            )
+            content_resp = await client.get(leaf["url"])
+            content = content_resp.json()
+            writable_content = base64.b64decode(content["content"]).decode("utf-8")
+            file_path.write_text(writable_content)
+
+
 async def main():
     async with httpx.AsyncClient(base_url="https://api.github.com/") as client:
         client.headers.update(
@@ -27,6 +55,7 @@ async def main():
             }
         )
         tree = await get_schema_tree(client, schema_request)
+        await write_schemas(client, tree)
 
 
 if __name__ == "__main__":
