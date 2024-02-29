c1de4ab05cf463f3b369bf7b32c30d3acee2785b
Mike Spiegel <mspiegel31@gmail.com>
2024-02-28 21:00:36 -0500

    more cleanup

--- backstage_catalog_client/api_client.py
+++ backstage_catalog_client/api_client.py
@@ -31,6 +31,8 @@ class CatalogApi(Protocol):
         options: CatalogRequestOptions | None = None,
     ) -> GetEntitiesResponse: ...
 
+
+class TODOCatalogApi(Protocol):
     async def getEntitiesByRefs(
         self,
         request: GetEntitiesByRefsRequest,
--- backstage_catalog_client/json_types.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-JsonPrimitive = int | float | str | bool | None
-
-JsonObject = dict[str, Any]
-JsonArray = list[Any]
-
-JsonValue = JsonObject | JsonArray | JsonPrimitive
